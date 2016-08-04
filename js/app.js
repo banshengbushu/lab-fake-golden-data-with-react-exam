36bdac3129bc4ea91048af120fa662cf26f351b3
banshengbushu <wangdanna1995@outlook.com>
2016-08-04 22:44:45 +0800

    Add element

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const App = React.createClass({
     getInitialState:function () {
       return({
           isEditor:true,
+          elements:[]
       })
     },
     toggle:function () {
@@ -10,18 +11,31 @@ const App = React.createClass({
           isEditor:!this.state.isEditor,
       })
     },
+
+    addElement:function (element) {
+      const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements})
+    },
     render:function(){
         const isEditor= this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"Preview":"Edit"}</button>
-            <Editor/>
+            <div className={isEditor?"":"hidden"}>
+            <Editor onAdd={this.addElement} elements ={this.state.elements}/>
+            </div >
+                <div className={isEditor?"hidden":""}>
             <Preview/>
+            </div>
         </div>
     }
 });
 const Editor = React.createClass({
     render:function(){
-        return <div>Editor</div>
+        return <div>
+            <Left elements ={this.props.elements}/>
+            <Right onAdd ={this.props.onAdd}/>
+        </div>
     }
 });
 const Preview = React.createClass({
@@ -30,14 +44,34 @@ const Preview = React.createClass({
     }
 });
 const Left = React.createClass({
+
     render:function(){
-        return <div>Left</div>
-    }
+        const elements = this.props.elements.map((ele,index)=>{
+            if(ele!==undefined) {
+                return <div key={index}>
+                    <input type={ele}/>
+                    <button>-</button>
+                </div>
+            }
+
+            });
+        return <div>{elements}</div>
+    },
+
 });
 
 const Right = React.createClass({
+
+    add:function () {
+      const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
     render:function(){
-        return <div>Right</div>
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
     }
 });
 

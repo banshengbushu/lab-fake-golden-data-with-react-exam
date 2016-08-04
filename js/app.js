0911eb11cee5719878b1b0184d0cd52bc5ea830e
banshengbushu <wangdanna1995@outlook.com>
2016-08-04 23:36:02 +0800

    Add element

--- js/app.js
+++ js/app.js
@@ -13,12 +13,18 @@ const App = React.createClass({
           })
     },
 
+    addElement:function (element) {
+      const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+
     render:function(){
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"Preview":"Edit"}</button>
             <div className={isEditor?"":"hidden"}>
-            <Editor/>
+            <Editor onAdd={this.addElement} elements={this.state.elements}/>
                 </div>
             <div className={isEditor?"hidden":""}>
             <Previewer/>
@@ -30,8 +36,8 @@ const App = React.createClass({
 const Editor = React.createClass({
     render:function(){
         return <div>
-            <Left/>
-            <Right/>
+            <Left elements={this.props.elements}/>
+            <Right onAdd = {this.props.onAdd}/>
         </div>
     }
 });
@@ -42,14 +48,30 @@ const Previewer = React.createClass({
 });
 
 const Right = React.createClass({
+    add:function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element)
+    },
     render:function(){
-        return <div>Right</div>
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
     }
 });
 
 const Left = React.createClass({
     render:function(){
-        return <div>Left</div>
+        const elements = this.props.elements.map((ele,index)=>{
+            return<div>
+                <input type={ele}/>
+                <button>-</button>
+            </div>
+        });
+        return <div>
+            {elements}
+        </div>
     }
 });
 ReactDOM.render(<App />, document.getElementById('content'));

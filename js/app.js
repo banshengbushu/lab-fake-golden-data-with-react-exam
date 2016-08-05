7081bf469fb01915b3830c66043df36b02257f4f
banshengbushu <wangdanna1995@outlook.com>
2016-08-05 09:58:38 +0800

    add Element

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 const App = React.createClass({
 
-    getInitialAState:function () {
-      return{
-          isEditor:true,
-          elements:[],
-      }
+    getInitialState: function() {
+        return {
+            isEditor: true,
+            elements: []
+        }
     },
-
-    toggle:function () {
-        return({
-            isEditor:!this.state.isEditor,
-        })
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        });
+    },
+    addElement: function(element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
     },
     render:function(){
         const isEditor= this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"Preview":"Edit"}</button>
             <div className={isEditor?"":"hidden"}>
-            <Editor/>
+            <Editor onAdd={this.addElement} elements={this.state.elements}/>
                 </div>
             <div className={isEditor?"hidden":""}>
             <Previewer/>
@@ -29,8 +33,8 @@ const App = React.createClass({
 const Editor = React.createClass({
     render:function(){
         return <div>
-            <Left/>
-            <Right/>
+            <Left elements={this.props.elements}/>
+            <Right onAdd={this.porps.onAdd}/>
         </div>
     }
 });
@@ -42,13 +46,31 @@ const Previewer = React.createClass({
 
 const Left = React.createClass({
     render:function(){
-        return <div>Left</div>
+        const elements=this.props.elements.map((ele,index)=>{
+            return<div>
+                <input type={ele}/>
+                <button>-</button>
+            </div>
+
+        });
+        return <div>
+            {elements}
+        </div>
     }
 });
 
 const Right = React.createClass({
+    add:function () {
+      const element =$("input[name=element]").val();
+        this.props.onAdd(element)
+    },
+
     render:function(){
-        return <div></div>
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>-</button>
+        </div>
     }
 });
 ReactDOM.render(<App />, document.getElementById('content'));
